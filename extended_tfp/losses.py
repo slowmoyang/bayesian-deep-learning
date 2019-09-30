4bfab9a2868c1cff3c7324010a0d225dfeeb8123
seungjin-yang <seungjin.yang@cern.ch>
2019-09-30 21:27:44 +0900

    Rename an argument

--- extended_tfp/losses.py
+++ extended_tfp/losses.py
@@ -3,8 +3,8 @@ import tensorflow_probability as tfp
 
 tfd = tfp.distributions
 
-def negative_log_likelihood(y_true, y_pred_rv):
+def negative_log_likelihood(y_true, y_pred_distribution):
     '''
     stole from https://github.com/tensorflow/probability/blob/master/tensorflow_probability/examples/jupyter_notebooks/Probabilistic_Layers_Regression.ipynb
     '''
-    return -y_pred_rv.log_prob(y_true)
+    return -y_pred_distribution.log_prob(y_true)
