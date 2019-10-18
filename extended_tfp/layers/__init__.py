ecbc5a412f83da1c35f3b114d42e3e93e11679f0
seungjin-yang <seungjin.yang@cern.ch>
2019-10-18 18:33:47 +0900

    Add variational multi-head attention layer

--- extended_tfp/layers/__init__.py
+++ extended_tfp/layers/__init__.py
@@ -1,3 +1,5 @@
 from extended_tfp.layers.conv_variational_v2 import Conv2DVariational
+from extended_tfp.layers.attention_variational import AttentionVariational
+from extended_tfp.layers.attention_variational import SelfAttentionVariational
 from extended_tfp.layers.utils import posterior_mean_field
 from extended_tfp.layers.utils import prior_trainable
